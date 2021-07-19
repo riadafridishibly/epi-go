b79d0674089b9e5446e21797cce131fbaa8f07ca
Riad Afridi Shibly <riadafridishibly@gmail.com>
2021-07-20 02:54:30 +0600

    Add ParityRemoveLowestBit

--- ch4/parity.go
+++ ch4/parity.go
@@ -31,11 +31,32 @@ package ch4
 //  res will be fliped n number of times, n is the number of ones
 //  starting from 0, if it flips even number of times then it'll be
 //  back to 0, and if it flips odd number of times, it'll be 1
+//
+//  Time Complexity, O(n), n is the int size
 func ParityNaive(x int) int {
 	res := 0
 	for x != 0 {
 		res ^= x & 1
 		x >>= 1
+	}
+
+	return res
+}
+
+// ParityRemoveLowestSetBit explanation
+//  From ParityNaive we came to learn that we need to flip the res
+//  n times, where n is the number of set bit. Here we're doing just
+//  that.
+//
+//  Previously for input like `0b1001` we had to shift bit 4 times,
+//  but now it's only two times
+//
+//  Time complexity, O(k), k is the number of set bit in the int
+func ParityRemoveLowestSetBit(x int) int {
+	res := 0
+	for x != 0 {
+		res ^= 1
+		x &= x - 1 // This line clears the lowest set bit
 	}
 
 	return res
--- ch4/parity_test.go
+++ ch4/parity_test.go
@@ -1,6 +1,8 @@
 package ch4
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestParityNaive(t *testing.T) {
 	type args struct {
@@ -26,6 +28,40 @@ func TestParityNaive(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ParityNaive(tt.args.x); got != tt.want {
 				t.Errorf("ParityNaive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParityRemoveLowestSetBit(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Parity of 7",
+			args: args{x: 7},
+			want: 1,
+		},
+		{
+			name: "Parity of 6",
+			args: args{x: 6},
+			want: 0,
+		},
+		{
+			name: "Parity of 9",
+			args: args{x: 9},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParityRemoveLowestSetBit(tt.args.x); got != tt.want {
+				t.Errorf("ParityRemoveLowestSetBit() = %v, want %v", got, tt.want)
 			}
 		})
 	}
